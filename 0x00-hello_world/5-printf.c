b62e8ef205835e234c6a48d5cf35b6994ead53fa
root <MaureenWanjiku>
2022-03-10 08:54:32 -0800

    seventh commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *
  *  * main - prints the phrase with proper grammar,
@@ -10,7 +9,6 @@
  *    * Return: 0 if exited properly, non-zero otherwise
  *
  *     */
-
 int main(void)
 
 {

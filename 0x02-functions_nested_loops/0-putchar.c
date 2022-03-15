cf3386ead75c000cec2767c59148114232239844
root <MaureenWanjiku>
2022-03-15 06:34:20 -0700

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
+#include <unistd.h>
 #include "main.h"
-#include<stdio.h>
-
-/**
-* main - block code
-* Return: 0.
+/**main - Putchar
+ * _putchar print character
+ *Return:Always 0 (success)
 */
 int main(void)
 {
-char *c = "_putchar";
-int i;
-for (i = 0; i < 8; i++)
-{
-_putchar(c[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('U');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r')
 _putchar('\n');
 return (0);
 }

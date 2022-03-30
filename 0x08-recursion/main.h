80cf76f1baca2c0c43c17440bdf193870d6bd909
root <MaureenWanjiku>
2022-03-30 13:50:41 -0700

    added 0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+void _puts_recursion(char *s);
+int _putchar(char c);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+#endif
